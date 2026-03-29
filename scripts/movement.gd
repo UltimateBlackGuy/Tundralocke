53d59af00cc251b4f3b0f3d5b2ca7edccb69b221
UltimateBlackGuy <kcgermain14@gmail.com>
2026-03-28 22:16:45 -0400

    Added some player sprites

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -9,13 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	if Input.is_action_pressed("sprint"):
-		speed = 450
-		$ColorRect.color = Color.RED
-	
-	if Input.is_action_just_released("sprint"):
-		speed = 350
-		$ColorRect.color = Color.WHITE
 	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
