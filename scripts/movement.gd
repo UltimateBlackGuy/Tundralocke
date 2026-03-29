e58738ed44212030b8b08213723fc326d0b5d476
UltimateBlackGuy <kcgermain14@gmail.com>
2026-03-29 12:31:26 -0400

    Added some frames for directionaly movement
    this was so ez

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed: int = 350
+@onready var sprite = $AnimatedSprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +10,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
+	
+	if  Input.is_action_pressed("left"):
+		sprite.frame = 1
+
+	if  Input.is_action_pressed("right"):
+		sprite.frame = 2
+	
+	if  Input.is_action_pressed("up"):
+		sprite.frame = 3
+	
+	if  Input.is_action_pressed("down"):
+		sprite.frame = 0
+	
