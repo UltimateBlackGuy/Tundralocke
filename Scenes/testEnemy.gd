91610a83158394e45cbafc62d92d76c96ca467e9
UltimateBlackGuy <kcgermain14@gmail.com>
2026-03-30 16:42:14 -0400

    Finished Wandering Behavior
    thanks to the big GPT for the math calculations

--- Scenes/testEnemy.gd
+++ Scenes/testEnemy.gd
@@ -2,39 +2,54 @@ class_name Enemy
 extends CharacterBody2D
 
 enum STATES {Idle, Wandering, Chasing} 
-var currentState = STATES.Idle
+var currentState = STATES.Wandering
 
 #don't know why I need to use enum but it works
 
 @export var speed: int
 @export var NavAgent: NavigationAgent2D
+@export var wander_area: Area2D
 
 var target_node = null
 
 func _ready():
-	pass
+	randomize()
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	 #will set up the states soon enough just lazy rn also the FUCK is elif
 	if currentState == STATES.Idle:
 		pass 
 	elif currentState == STATES.Wandering:
-		pass
+		if NavAgent.is_navigation_finished(): #so it doesn't refresh every frame
+			var random_point = get_random_point_in_wander_area()
+			NavAgent.target_position = random_point
+		move_along_path()
 	elif currentState == STATES.Chasing:
 		pass
 	
 
-func makePath(navAgent):
-	pass
+func move_along_path():
+	var dir = to_local(NavAgent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()
+
+func get_random_point_in_wander_area():
+	var shape = wander_area.get_node("CollisionShape2D").shape
+	var radius = shape.radius
+
+	var angle = randf_range(0, TAU) #Tau is just the full circle
+	var r = radius * sqrt(randf()) #so it won't be close to the center
+
+	var offset = Vector2(cos(angle), sin(angle)) * r #jumbled math bs
+	return wander_area.global_position + offset #returns the random point for use
 
 func _on_aggro_range_area_entered(area) -> void:
 	if area is Player or area is Alurian:
 		currentState = STATES.Chasing
 
-
-func _on_de_aggro_range_area_entered(area) -> void:
+func _on_de_aggro_range_area_exited(area) -> void:
 	if area is Player or area is Alurian:
-		currentState = STATES.Wandering
+		currentState = STATES.Idle
 
 
 func _on_recalc_navigation_timeout() -> void:
